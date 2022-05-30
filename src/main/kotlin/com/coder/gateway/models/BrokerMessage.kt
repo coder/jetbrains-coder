80dd5b23261be34a907802c36646de403318c478
Ioan Faur <fioan89@gmail.com>
2022-05-30 23:53:16 +0300

    More data in the broker message
    - add docs for the new data types and properties
    - new DialPolicy data type

--- src/main/kotlin/com/coder/gateway/models/BrokerMessage.kt
+++ src/main/kotlin/com/coder/gateway/models/BrokerMessage.kt
@@ -4,4 +4,44 @@ import com.google.gson.annotations.SerializedName
 import dev.onvoid.webrtc.RTCIceServer
 import dev.onvoid.webrtc.RTCSessionDescription
 
-data class BrokerMessage(@SerializedName("offer") val offer: RTCSessionDescription, @SerializedName("servers") val servers: List<RTCIceServer>, @SerializedName("turn_proxy_url") val turnProxyUrl: String)
+/**
+ * BrokerMessage is used for brokering a dialer and listener.
+ *
+ * Dialers initiate an exchange by providing an Offer, along with a list of ICE servers for the listener to peer with.
+ * The listener should respond with an offer, then both sides can begin exchanging candidates.
+ *
+ */
+data class BrokerMessage(
+    // Dialer -> Listener
+    @SerializedName("offer") val offer: RTCSessionDescription,
+    @SerializedName("servers") val servers: List<RTCIceServer>,
+    @SerializedName("turn_proxy_url") val turnProxyUrl: String,
+    @SerializedName("ports") val ports: Array<DialPolicy>? = null,
+    // Listener -> Dialer
+    @SerializedName("error") val error: String? = "",
+    @SerializedName("answer") val answer: RTCSessionDescription? = null,
+    // Bidirectional
+    @SerializedName("candidate") val candidate: String? = ""
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as BrokerMessage
+
+        if (offer != other.offer) return false
+        if (servers != other.servers) return false
+        if (turnProxyUrl != other.turnProxyUrl) return false
+        if (!ports.contentEquals(other.ports)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = offer.hashCode()
+        result = 31 * result + servers.hashCode()
+        result = 31 * result + turnProxyUrl.hashCode()
+        result = 31 * result + ports.contentHashCode()
+        return result
+    }
+}
---
+++ src/main/kotlin/com/coder/gateway/models/DialPolicy.kt
@@ -0,0 +1,12 @@
+package com.coder.gateway.models
+
+import com.google.gson.annotations.SerializedName
+
+/**
+ * DialPolicy a single network + address + port combinations that a connection is permitted to use.
+ * @param network if empty it applies to all networks
+ * @param host the IP or hostname of the address. It should not contain the port. If empty, it applies to all hosts. "localhost", [::1], and any IPv4
+ * address under "127.0.0.0/8" can be used interchangeably.
+ * @param port it applies to all ports if value is 0
+ */
+data class DialPolicy(@SerializedName("network") val network: String?, @SerializedName("address") val host: String?, @SerializedName("port") val port: UShort?)
