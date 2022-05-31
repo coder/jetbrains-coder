45bb9a56dfbb487967d1608ac8bc0e088b1e1093
Ioan Faur <fioan89@gmail.com>
2022-05-31 22:08:23 +0300

    Make optional all broker properties

--- src/main/kotlin/com/coder/gateway/models/BrokerMessage.kt
+++ src/main/kotlin/com/coder/gateway/models/BrokerMessage.kt
@@ -13,9 +13,9 @@ import dev.onvoid.webrtc.RTCSessionDescription
  */
 data class BrokerMessage(
     // Dialer -> Listener
-    @SerializedName("offer") val offer: RTCSessionDescription,
-    @SerializedName("servers") val servers: List<RTCIceServer>,
-    @SerializedName("turn_proxy_url") val turnProxyUrl: String,
+    @SerializedName("offer") val offer: RTCSessionDescription? = null,
+    @SerializedName("servers") val servers: List<RTCIceServer>? = null,
+    @SerializedName("turn_proxy_url") val turnProxyUrl: String? = "",
     @SerializedName("ports") val ports: Array<DialPolicy>? = null,
     // Listener -> Dialer
     @SerializedName("error") val error: String? = "",
@@ -32,16 +32,25 @@ data class BrokerMessage(
         if (offer != other.offer) return false
         if (servers != other.servers) return false
         if (turnProxyUrl != other.turnProxyUrl) return false
-        if (!ports.contentEquals(other.ports)) return false
+        if (ports != null) {
+            if (other.ports == null) return false
+            if (!ports.contentEquals(other.ports)) return false
+        } else if (other.ports != null) return false
+        if (error != other.error) return false
+        if (answer != other.answer) return false
+        if (candidate != other.candidate) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        var result = offer.hashCode()
-        result = 31 * result + servers.hashCode()
-        result = 31 * result + turnProxyUrl.hashCode()
-        result = 31 * result + ports.contentHashCode()
+        var result = offer?.hashCode() ?: 0
+        result = 31 * result + (servers?.hashCode() ?: 0)
+        result = 31 * result + (turnProxyUrl?.hashCode() ?: 0)
+        result = 31 * result + (ports?.contentHashCode() ?: 0)
+        result = 31 * result + (error?.hashCode() ?: 0)
+        result = 31 * result + (answer?.hashCode() ?: 0)
+        result = 31 * result + (candidate?.hashCode() ?: 0)
         return result
     }
 }
