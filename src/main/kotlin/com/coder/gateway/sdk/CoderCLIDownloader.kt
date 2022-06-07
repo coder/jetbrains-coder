e4bebd86630b07a18931e5de81568c26d1cf6d47
Ioan Faur <fioan89@gmail.com>
2022-06-07 22:46:46 +0300

    Default to amd64 if arch can't be determined

--- src/main/kotlin/com/coder/gateway/sdk/CoderCLIDownloader.kt
+++ src/main/kotlin/com/coder/gateway/sdk/CoderCLIDownloader.kt
@@ -5,14 +5,20 @@ import java.net.URL
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.StandardCopyOption
+import java.util.logging.Logger
 
 class CoderCLIDownloader {
 
     fun downloadCLI(url: URL, outputName: String, ext: String): Path {
         val tempPath: Path = Files.createTempFile(outputName, ext)
+        logger.info("Downloading $url to $tempPath")
         url.openStream().use {
             Files.copy(it as InputStream, tempPath, StandardCopyOption.REPLACE_EXISTING)
         }
         return tempPath
+    }
+
+    companion object {
+        val logger = Logger.getLogger(CoderCLIDownloader::class.java.simpleName)
     }
 }
--- src/main/kotlin/com/coder/gateway/sdk/CoderCLIManager.kt
+++ src/main/kotlin/com/coder/gateway/sdk/CoderCLIManager.kt
@@ -2,6 +2,7 @@ package com.coder.gateway.sdk
 
 import java.net.URL
 import java.nio.file.Path
+import java.util.logging.Logger
 
 class CoderCLIManager(private val url: URL) {
     private val coderCLIDownloader = CoderCLIDownloader()
@@ -9,30 +10,37 @@ class CoderCLIManager(private val url: URL) {
     fun download(): Path? {
         val os = getOS()
         val cliName = getCoderCLIForOS(os, getArch()) ?: return null
+        logger.info("Resolved OS: $os with arch: ${getArch()}")
         val cliNameWitExt = if (os == OS.WINDOWS) "$cliName.exe" else cliName
         return coderCLIDownloader.downloadCLI(URL(url.protocol, url.host, url.port, "/bin/$cliNameWitExt"), cliName, if (os == OS.WINDOWS) ".exe" else "")
     }
 
     fun getCoderCLIForOS(os: OS?, arch: Arch?): String? {
-        if (os == null || arch == null) {
+        logger.info("Resolving coder cli for $os $arch")
+        if (os == null) {
             return null
         }
         return when (os) {
             OS.WINDOWS -> when (arch) {
                 Arch.amd64 -> "coder-windows-amd64"
                 Arch.arm64 -> "coder-windows-arm64"
-                else -> null
+                else -> "coder-windows-amd64"
             }
             OS.LINUX -> when (arch) {
                 Arch.amd64 -> "coder-linux-amd64"
                 Arch.arm64 -> "coder-linux-arm64"
                 Arch.armv7 -> "coder-linux-armv7"
+                else -> "coder-linux-amd64"
             }
             OS.MAC -> when (arch) {
                 Arch.amd64 -> "coder-darwin-amd64"
                 Arch.arm64 -> "coder-darwin-arm64"
-                else -> null
+                else -> "coder-darwin-amd64"
             }
         }
+    }
+
+    companion object {
+        val logger = Logger.getLogger(CoderCLIManager::class.java.simpleName)
     }
 }
