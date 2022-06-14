f0aefac42487af9d0328dbb325da75ec8a75b66f
Ioan Faur <fioan89@gmail.com>
2022-06-15 00:00:34 +0300

    Impl: rewrite recent workspace view without JTree
    - JTree catches all mouse actions, and it needs heavy customization
      to delegate the events to the children
    - this boilerplate can be simply avoided by using the UI DSL to
      dynamically build the view

--- src/main/kotlin/com/coder/gateway/views/CoderGatewayRecentWorkspaceConnectionsView.kt
+++ src/main/kotlin/com/coder/gateway/views/CoderGatewayRecentWorkspaceConnectionsView.kt
@@ -2,105 +2,97 @@ package com.coder.gateway.views
 
 import com.coder.gateway.CoderGatewayBundle
 import com.coder.gateway.icons.CoderIcons
-import com.coder.gateway.models.RecentWorkspaceConnection
 import com.coder.gateway.services.CoderRecentWorkspaceConnectionsService
 import com.intellij.openapi.components.service
+import com.intellij.openapi.ui.panel.ComponentPanelBuilder
 import com.intellij.openapi.wm.impl.welcomeScreen.WelcomeScreenUIManager
-import com.intellij.ui.dsl.builder.TopGap
+import com.intellij.ui.components.ActionLink
+import com.intellij.ui.components.JBScrollPane
+import com.intellij.ui.dsl.builder.BottomGap
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.gridLayout.HorizontalAlign
-import com.intellij.ui.dsl.gridLayout.VerticalAlign
-import com.intellij.ui.treeStructure.Tree
-import com.intellij.ui.treeStructure.treetable.ListTreeTableModel
-import com.intellij.ui.treeStructure.treetable.TreeColumnInfo
 import com.intellij.util.ui.JBFont
+import com.intellij.util.ui.JBUI
 import com.jetbrains.gateway.api.GatewayRecentConnections
+import com.jetbrains.gateway.api.GatewayUI
+import com.jetbrains.gateway.ssh.IntelliJPlatformProduct
 import com.jetbrains.rd.util.lifetime.Lifetime
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import javax.swing.JComponent
-import javax.swing.JTree
-import javax.swing.tree.DefaultMutableTreeNode
-import javax.swing.tree.TreeNode
-import javax.swing.tree.TreePath
 
 class CoderGatewayRecentWorkspaceConnectionsView : GatewayRecentConnections {
     private val recentConnectionsService = service<CoderRecentWorkspaceConnectionsService>()
+    private val cs = CoroutineScope(Dispatchers.Main)
 
-    private var top = DefaultMutableTreeNode()
-    private var recentConnectionsTeeModel = ListTreeTableModel(top, emptyArray<TreeColumnInfo>())
-    private val recentConnectionsView = Tree(recentConnectionsTeeModel)
+    private var contentPanel = JBScrollPane()
 
     override val id = "CoderGatewayRecentConnections"
 
     override val recentsIcon = CoderIcons.LOGO_16
 
     override fun createRecentsView(lifetime: Lifetime): JComponent {
-        val view = panel {
-            indent {
-                row {
-                    label(CoderGatewayBundle.message("gateway.connector.recentconnections.title")).applyToComponent {
-                        font = JBFont.h3().asBold()
-                    }
-                }
-                row {
-                    scrollCell(recentConnectionsView).resizableColumn().horizontalAlign(HorizontalAlign.FILL).verticalAlign(VerticalAlign.FILL).applyToComponent {
-                        background = WelcomeScreenUIManager.getMainAssociatedComponentBackground()
-                    }
-                    cell()
-                }.topGap(TopGap.NONE).resizableRow()
-            }
-        }.apply {
-            background = WelcomeScreenUIManager.getMainAssociatedComponentBackground()
-        }
-
-        recentConnectionsView.apply {
-            isRootVisible = false
-            showsRootHandles = false
-            rowHeight = -1
-            val mouseListeners = mouseListeners
-            for (mouseListener in mouseListeners) {
-                removeMouseListener(mouseListener)
-            }
-            cellRenderer = CoderRecentWorkspaceConnectionsTreeRenderer()
-        }
-        updateTree()
-        return view
+        updateContentView()
+        return contentPanel
     }
 
     override fun getRecentsTitle() = CoderGatewayBundle.message("gateway.connector.title")
 
     override fun updateRecentView() {
-        updateTree()
+        updateContentView()
     }
 
-    private fun updateTree() {
+    private fun updateContentView() {
         val groupedConnections = recentConnectionsService.getAllRecentConnections().groupBy { it.coderWorkspaceHostname }
-        top.removeAllChildren()
-        groupedConnections.entries.forEach { (hostname, recentConnections) ->
-            val hostnameTreeNode = HostnameTreeNode(hostname)
-            recentConnections.forEach { connectionDetails ->
-                hostnameTreeNode.add(ProjectTreeNode(connectionDetails))
-            }
-            top.add(hostnameTreeNode)
-        }
+        contentPanel.viewport.view = panel {
+            indent {
+                row {
+                    label(CoderGatewayBundle.message("gateway.connector.recentconnections.title")).applyToComponent {
+                        font = JBFont.h3().asBold()
+                    }
+                }.bottomGap(BottomGap.MEDIUM)
+                groupedConnections.entries.forEach { (hostname, recentConnections) ->
+                    row {
+                        if (hostname != null) {
+                            label(hostname).applyToComponent {
+                                font = JBFont.h3().asBold()
+                            }.horizontalAlign(HorizontalAlign.LEFT)
+                            cell()
+                        }
+                    }
 
-        expandAll(recentConnectionsView, TreePath(top))
-    }
-
-    private fun expandAll(tree: JTree, parent: TreePath) {
-        val node: TreeNode = parent.lastPathComponent as TreeNode
-        if (node.childCount >= 0) {
-            val e = node.children()
-            while (e.hasMoreElements()) {
-                val n: TreeNode = e.nextElement() as TreeNode
-                val path: TreePath = parent.pathByAddingChild(n)
-                expandAll(tree, path)
+                    group {
+                        recentConnections.forEach { connectionDetails ->
+                            val product = IntelliJPlatformProduct.fromProductCode(connectionDetails.ideProductCode!!)!!
+                            row {
+                                icon(product.icon)
+                                cell(ActionLink(connectionDetails.projectPath!!) {
+                                    cs.launch {
+                                        GatewayUI.getInstance().connect(
+                                            mapOf(
+                                                "type" to "coder",
+                                                "coder_workspace_hostname" to "${connectionDetails.coderWorkspaceHostname}",
+                                                "project_path" to connectionDetails.projectPath!!,
+                                                "ide_product_code" to "${product.productCode}",
+                                                "ide_build_number" to "${connectionDetails.ideBuildNumber}",
+                                                "ide_download_link" to "${connectionDetails.downloadSource}"
+                                            )
+                                        )
+                                    }
+                                })
+                                label("").resizableColumn().horizontalAlign(HorizontalAlign.FILL)
+                                label("Last opened: ${connectionDetails.lastOpened}").horizontalAlign(HorizontalAlign.RIGHT).applyToComponent {
+                                    foreground = JBUI.CurrentTheme.ContextHelp.FOREGROUND
+                                    font = ComponentPanelBuilder.getCommentFont(font)
+                                }
+                            }
+                        }
+                    }
+                }
             }
+        }.apply {
+            background = WelcomeScreenUIManager.getMainAssociatedComponentBackground()
         }
-        tree.expandPath(parent)
     }
 }
-
-
-class HostnameTreeNode(val hostname: String?) : DefaultMutableTreeNode()
-
-class ProjectTreeNode(val connectionDetails: RecentWorkspaceConnection) : DefaultMutableTreeNode()
