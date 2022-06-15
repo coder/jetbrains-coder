30c8ac53b4a29ca2b0ef02fdf845fb65244ee82f
Ioan Faur <fioan89@gmail.com>
2022-06-15 22:47:11 +0300

    Impl: Recent Workspaces search functionality
    - it can search by hostname and by project path
    - it's not case sensitive
    - re-worked some of the code building the UI. The
      first part - up to the list of workspaces - is now
      statically declared and created only once.

--- src/main/kotlin/com/coder/gateway/views/CoderGatewayRecentWorkspaceConnectionsView.kt
+++ src/main/kotlin/com/coder/gateway/views/CoderGatewayRecentWorkspaceConnectionsView.kt
@@ -2,6 +2,7 @@ package com.coder.gateway.views
 
 import com.coder.gateway.CoderGatewayBundle
 import com.coder.gateway.icons.CoderIcons
+import com.coder.gateway.models.RecentWorkspaceConnection
 import com.coder.gateway.services.CoderRecentWorkspaceConnectionsService
 import com.intellij.ide.BrowserUtil
 import com.intellij.openapi.actionSystem.AnActionEvent
@@ -9,6 +10,8 @@ import com.intellij.openapi.components.service
 import com.intellij.openapi.project.DumbAwareAction
 import com.intellij.openapi.ui.panel.ComponentPanelBuilder
 import com.intellij.openapi.wm.impl.welcomeScreen.WelcomeScreenUIManager
+import com.intellij.ui.DocumentAdapter
+import com.intellij.ui.SearchTextField
 import com.intellij.ui.components.ActionLink
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.dsl.builder.BottomGap
@@ -16,6 +19,7 @@ import com.intellij.ui.dsl.builder.RightGap
 import com.intellij.ui.dsl.builder.TopGap
 import com.intellij.ui.dsl.builder.panel
 import com.intellij.ui.dsl.gridLayout.HorizontalAlign
+import com.intellij.ui.dsl.gridLayout.VerticalAlign
 import com.intellij.util.ui.JBFont
 import com.intellij.util.ui.JBUI
 import com.jetbrains.gateway.api.GatewayRecentConnections
@@ -25,89 +29,110 @@ import com.jetbrains.rd.util.lifetime.Lifetime
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import java.awt.Dimension
 import javax.swing.JComponent
+import javax.swing.event.DocumentEvent
 
 class CoderGatewayRecentWorkspaceConnectionsView : GatewayRecentConnections {
     private val recentConnectionsService = service<CoderRecentWorkspaceConnectionsService>()
     private val cs = CoroutineScope(Dispatchers.Main)
 
-    private var contentPanel = JBScrollPane()
+    private val contentPanel = JBScrollPane()
+
+    private lateinit var searchBar: SearchTextField
 
     override val id = "CoderGatewayRecentConnections"
 
     override val recentsIcon = CoderIcons.LOGO_16
 
     override fun createRecentsView(lifetime: Lifetime): JComponent {
-        updateContentView()
-        return contentPanel
-    }
-
-    override fun getRecentsTitle() = CoderGatewayBundle.message("gateway.connector.title")
-
-    override fun updateRecentView() {
-        updateContentView()
-    }
-
-    private fun updateContentView() {
-        val groupedConnections = recentConnectionsService.getAllRecentConnections().groupBy { it.coderWorkspaceHostname }
-        contentPanel.viewport.view = panel {
+        return panel {
             indent {
                 row {
                     label(CoderGatewayBundle.message("gateway.connector.recentconnections.title")).applyToComponent {
                         font = JBFont.h3().asBold()
                     }
+                    searchBar = cell(SearchTextField(false)).applyToComponent {
+                        minimumSize = Dimension(350, -1)
+                        textEditor.border = JBUI.Borders.empty(2, 5, 2, 0)
+                    }.horizontalAlign(HorizontalAlign.RIGHT).component
+                    searchBar.addDocumentListener(object : DocumentAdapter() {
+                        override fun textChanged(e: DocumentEvent) {
+                            val toSearchFor = searchBar.text
+                            val filteredConnections = recentConnectionsService.getAllRecentConnections().filter { it.coderWorkspaceHostname?.toLowerCase()?.contains(toSearchFor) ?: false || it.projectPath?.toLowerCase()?.contains(toSearchFor) ?: false }
+                            updateContentView(filteredConnections.groupBy { it.coderWorkspaceHostname })
+                        }
+                    })
                 }.bottomGap(BottomGap.MEDIUM)
-                groupedConnections.entries.forEach { (hostname, recentConnections) ->
-                    row {
-                        if (hostname != null) {
-                            label(hostname).applyToComponent {
-                                font = JBFont.h3().asBold()
-                            }.horizontalAlign(HorizontalAlign.LEFT).gap(RightGap.SMALL)
-                            actionButton(object : DumbAwareAction("Open SSH Web Terminal", "", CoderIcons.OPEN_TERMINAL) {
-                                override fun actionPerformed(e: AnActionEvent) {
-                                    BrowserUtil.browse(recentConnections[0]?.webTerminalLink ?: "")
-                                }
-                            })
-                        }
-                    }.topGap(TopGap.MEDIUM)
+                separator(background = WelcomeScreenUIManager.getSeparatorColor())
+                row {
+                    resizableRow()
+                    cell(contentPanel).resizableColumn().horizontalAlign(HorizontalAlign.FILL).verticalAlign(VerticalAlign.FILL).component
+                }
+            }
+        }.apply {
+            background = WelcomeScreenUIManager.getMainAssociatedComponentBackground()
+            border = JBUI.Borders.empty(12, 0, 0, 12)
+        }
+    }
 
-                    recentConnections.forEach { connectionDetails ->
-                        val product = IntelliJPlatformProduct.fromProductCode(connectionDetails.ideProductCode!!)!!
-                        row {
-                            icon(product.icon)
-                            cell(ActionLink(connectionDetails.projectPath!!) {
-                                cs.launch {
-                                    GatewayUI.getInstance().connect(
-                                        mapOf(
-                                            "type" to "coder",
-                                            "coder_workspace_hostname" to "${connectionDetails.coderWorkspaceHostname}",
-                                            "project_path" to connectionDetails.projectPath!!,
-                                            "ide_product_code" to "${product.productCode}",
-                                            "ide_build_number" to "${connectionDetails.ideBuildNumber}",
-                                            "ide_download_link" to "${connectionDetails.downloadSource}",
-                                            "web_terminal_link" to "${connectionDetails.webTerminalLink}"
-                                        )
-                                    )
-                                }
-                            })
-                            label("").resizableColumn().horizontalAlign(HorizontalAlign.FILL)
-                            label("Last opened: ${connectionDetails.lastOpened}").applyToComponent {
-                                foreground = JBUI.CurrentTheme.ContextHelp.FOREGROUND
-                                font = ComponentPanelBuilder.getCommentFont(font)
+    override fun getRecentsTitle() = CoderGatewayBundle.message("gateway.connector.title")
+
+    override fun updateRecentView() {
+        updateContentView(recentConnectionsService.getAllRecentConnections().groupBy { it.coderWorkspaceHostname })
+    }
+
+    private fun updateContentView(groupedConnections: Map<String?, List<RecentWorkspaceConnection>>) {
+        contentPanel.viewport.view = panel {
+            groupedConnections.entries.forEach { (hostname, recentConnections) ->
+                row {
+                    if (hostname != null) {
+                        label(hostname).applyToComponent {
+                            font = JBFont.h3().asBold()
+                        }.horizontalAlign(HorizontalAlign.LEFT).gap(RightGap.SMALL)
+                        actionButton(object : DumbAwareAction("Open SSH Web Terminal", "", CoderIcons.OPEN_TERMINAL) {
+                            override fun actionPerformed(e: AnActionEvent) {
+                                BrowserUtil.browse(recentConnections[0]?.webTerminalLink ?: "")
                             }
-                            actionButton(object : DumbAwareAction("Remove", "", CoderIcons.DELETE) {
-                                override fun actionPerformed(e: AnActionEvent) {
-                                    recentConnectionsService.removeConnection(connectionDetails)
-                                    updateContentView()
-                                }
-                            })
+                        })
+                    }
+                }.topGap(TopGap.MEDIUM)
+
+                recentConnections.forEach { connectionDetails ->
+                    val product = IntelliJPlatformProduct.fromProductCode(connectionDetails.ideProductCode!!)!!
+                    row {
+                        icon(product.icon)
+                        cell(ActionLink(connectionDetails.projectPath!!) {
+                            cs.launch {
+                                GatewayUI.getInstance().connect(
+                                    mapOf(
+                                        "type" to "coder",
+                                        "coder_workspace_hostname" to "${connectionDetails.coderWorkspaceHostname}",
+                                        "project_path" to connectionDetails.projectPath!!,
+                                        "ide_product_code" to "${product.productCode}",
+                                        "ide_build_number" to "${connectionDetails.ideBuildNumber}",
+                                        "ide_download_link" to "${connectionDetails.downloadSource}"
+                                    )
+                                )
+                            }
+                        })
+                        label("").resizableColumn().horizontalAlign(HorizontalAlign.FILL)
+                        label("Last opened: ${connectionDetails.lastOpened}").applyToComponent {
+                            foreground = JBUI.CurrentTheme.ContextHelp.FOREGROUND
+                            font = ComponentPanelBuilder.getCommentFont(font)
                         }
+                        actionButton(object : DumbAwareAction("Remove from Recent Connections", "", CoderIcons.DELETE) {
+                            override fun actionPerformed(e: AnActionEvent) {
+                                recentConnectionsService.removeConnection(connectionDetails)
+                                updateContentView(recentConnectionsService.getAllRecentConnections().groupBy { it.coderWorkspaceHostname })
+                            }
+                        })
                     }
                 }
             }
         }.apply {
             background = WelcomeScreenUIManager.getMainAssociatedComponentBackground()
-            border = JBUI.Borders.empty(0, 0, 0, 12)
+            border = JBUI.Borders.empty(12, 0, 0, 12)
         }
     }
 }
