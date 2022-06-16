299b260439e24287ab9bb32db9bd1cb347a2cfa6
Ioan Faur <fioan89@gmail.com>
2022-06-16 23:31:25 +0300

    Fix: detect arch on Mac OS X
    - on Mac OS X running on Intel proc, the arch is not correctly detected.
      That is because the jdk implementation reports `x86_64` instead of
      `amd64`
    - also, https://www.mail-archive.com/dev@netbeans.apache.org/msg08753.html
      reports arch as `aarch64` on Mac OS running on M1
    - resolves #9

--- src/main/kotlin/com/coder/gateway/sdk/os.kt
+++ src/main/kotlin/com/coder/gateway/sdk/os.kt
@@ -3,13 +3,13 @@ package com.coder.gateway.sdk
 fun getOS(): OS? {
     val os = System.getProperty("os.name").toLowerCase()
     return when {
-        os.contains("win") -> {
+        os.contains("win", true) -> {
             OS.WINDOWS
         }
-        os.contains("nix") || os.contains("nux") || os.contains("aix") -> {
+        os.contains("nix", true) || os.contains("nux", true) || os.contains("aix", true) -> {
             OS.LINUX
         }
-        os.contains("mac") -> {
+        os.contains("mac", true) -> {
             OS.MAC
         }
         else -> null
@@ -19,9 +19,9 @@ fun getOS(): OS? {
 fun getArch(): Arch? {
     val arch = System.getProperty("os.arch").toLowerCase()
     return when {
-        arch.contains("amd64") -> Arch.amd64
-        arch.contains("arm64") -> Arch.arm64
-        arch.contains("armv7") -> Arch.armv7
+        arch.contains("amd64", true) || arch.contains("x86_64", true) -> Arch.amd64
+        arch.contains("arm64", true) || arch.contains("aarch64", true) -> Arch.arm64
+        arch.contains("armv7", true) -> Arch.armv7
         else -> null
     }
 }
