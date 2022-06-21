f5fb90a51095338ee1d135db0117ee2d28218a1c
Ioan Faur <fioan89@gmail.com>
2022-06-21 22:21:21 +0300

    Delete unused coder V1 models

--- src/main/kotlin/com/coder/gateway/sdk/v1/LoginRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.coder.gateway.sdk.v1
-
-import com.google.gson.annotations.SerializedName
-
-data class LoginRequest(
-    @SerializedName("email") val email: String,
-    @SerializedName("password") val password: String
-)
