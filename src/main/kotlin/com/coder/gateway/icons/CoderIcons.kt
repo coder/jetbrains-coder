3e26a3cd53277ddb78c7c92cbc142333cb94875c
Faur Ioan-Aurel <fioan89@gmail.com>
2022-12-08 01:17:38 +0200

    chore: remove unused svgs

--- src/main/kotlin/com/coder/gateway/icons/CoderIcons.kt
+++ src/main/kotlin/com/coder/gateway/icons/CoderIcons.kt
@@ -5,7 +5,6 @@ import com.intellij.openapi.util.IconLoader
 object CoderIcons {
     val LOGO = IconLoader.getIcon("coder_logo.svg", javaClass)
     val LOGO_16 = IconLoader.getIcon("coder_logo_16.svg", javaClass)
-    val LOGO_52 = IconLoader.getIcon("coder_logo_52.svg", javaClass)
 
     val OPEN_TERMINAL = IconLoader.getIcon("open_terminal.svg", javaClass)
 
@@ -13,17 +12,9 @@ object CoderIcons {
     val RUN = IconLoader.getIcon("run.svg", javaClass)
     val STOP = IconLoader.getIcon("stop.svg", javaClass)
     val UPDATE = IconLoader.getIcon("update.svg", javaClass)
-
-    val WINDOWS = IconLoader.getIcon("windows.svg", javaClass)
-    val MACOS = IconLoader.getIcon("macOS.svg", javaClass)
-    val LINUX = IconLoader.getIcon("linux.svg", javaClass)
-    val UNKNOWN = IconLoader.getIcon("unknown.svg", javaClass)
-
-    val GREEN_CIRCLE = IconLoader.getIcon("green_circle.svg", javaClass)
-    val GRAY_CIRCLE = IconLoader.getIcon("gray_circle.svg", javaClass)
-    val RED_CIRCLE = IconLoader.getIcon("red_circle.svg", javaClass)
-
     val DELETE = IconLoader.getIcon("delete.svg", javaClass)
+
+    val UNKNOWN = IconLoader.getIcon("unknown.svg", javaClass)
 
     val ZERO = IconLoader.getIcon("0.svg", javaClass)
     val ONE = IconLoader.getIcon("1.svg", javaClass)
@@ -62,6 +53,5 @@ object CoderIcons {
     val X = IconLoader.getIcon("x.svg", javaClass)
     val Y = IconLoader.getIcon("y.svg", javaClass)
     val Z = IconLoader.getIcon("z.svg", javaClass)
-
 
 }
