a11ae830965eda5552313f08756acdb71ec8e7e9
Asher <ash@coder.com>
2023-04-21 16:40:07 -0800

    Surface token source and error
    Now it will say whether the token was from the config or was the last
    known token and if it fails there will be an error message.  You could
    always check the error in the bottom right but this way it is more
    obvious why the token dialog has reappeared.
    
    Also if the URL has changed there is no point trying to use the token
    we had stored for the previous URL.

--- src/main/kotlin/com/coder/gateway/models/CoderWorkspacesWizardModel.kt
+++ src/main/kotlin/com/coder/gateway/models/CoderWorkspacesWizardModel.kt
@@ -1,8 +1,14 @@
 package com.coder.gateway.models
 
+enum class TokenSource {
+    CONFIG,    // Pulled from the Coder CLI config.
+    USER,      // Input by the user.
+    LAST_USED, // Last used token, either from storage or current run.
+}
+
 data class CoderWorkspacesWizardModel(
     var coderURL: String = "https://coder.example.com",
-    var token: String = "",
+    var token: Pair<String, TokenSource>? = null,
     var selectedWorkspace: WorkspaceAgentModel? = null,
     var useExistingToken: Boolean = false,
 )
