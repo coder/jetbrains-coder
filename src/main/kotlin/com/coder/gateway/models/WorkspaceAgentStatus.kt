ed37ab0aed7de41955f42a05367a0f14e46e566c
Asher <ash@coder.com>
2023-04-25 12:11:34 -0800

    Simplify workspace status check
    Looks like there is already a property that is the composite of the two
    fields we were checking.

--- src/main/kotlin/com/coder/gateway/models/WorkspaceAgentStatus.kt
+++ src/main/kotlin/com/coder/gateway/models/WorkspaceAgentStatus.kt
@@ -1,8 +1,7 @@
 package com.coder.gateway.models
 
-import com.coder.gateway.sdk.v2.models.ProvisionerJobStatus
 import com.coder.gateway.sdk.v2.models.Workspace
-import com.coder.gateway.sdk.v2.models.WorkspaceTransition
+import com.coder.gateway.sdk.v2.models.WorkspaceStatus
 import com.intellij.ui.JBColor
 
 enum class WorkspaceAgentStatus(val label: String) {
@@ -16,24 +15,20 @@ enum class WorkspaceAgentStatus(val label: String) {
         else -> if (JBColor.isBright()) JBColor.LIGHT_GRAY else JBColor.DARK_GRAY
     }
 
+    // Note that latest_build.status is derived from latest_build.job.status and
+    // latest_build.job.transition so there is no need to check those.
     companion object {
-        fun from(workspace: Workspace) = when (workspace.latestBuild.job.status) {
-            ProvisionerJobStatus.PENDING -> QUEUED
-            ProvisionerJobStatus.RUNNING -> when (workspace.latestBuild.transition) {
-                WorkspaceTransition.START -> STARTING
-                WorkspaceTransition.STOP -> STOPPING
-                WorkspaceTransition.DELETE -> DELETING
-            }
-
-            ProvisionerJobStatus.SUCCEEDED -> when (workspace.latestBuild.transition) {
-                WorkspaceTransition.START -> RUNNING
-                WorkspaceTransition.STOP -> STOPPED
-                WorkspaceTransition.DELETE -> DELETED
-            }
-
-            ProvisionerJobStatus.CANCELING -> CANCELING
-            ProvisionerJobStatus.CANCELED -> CANCELED
-            ProvisionerJobStatus.FAILED -> FAILED
+        fun from(workspace: Workspace, agent: WorkspaceAgentModel) = when (workspace.latestBuild.status) {
+            WorkspaceStatus.PENDING -> QUEUED
+            WorkspaceStatus.STARTING -> STARTING
+            WorkspaceStatus.RUNNING -> RUNNING
+            WorkspaceStatus.STOPPING -> STOPPING
+            WorkspaceStatus.STOPPED -> STOPPED
+            WorkspaceStatus.FAILED -> FAILED
+            WorkspaceStatus.CANCELING -> CANCELING
+            WorkspaceStatus.CANCELED -> CANCELED
+            WorkspaceStatus.DELETING -> DELETING
+            WorkspaceStatus.DELETED -> DELETED
         }
 
         fun from(str: String) = WorkspaceAgentStatus.values().first { it.label.contains(str, true) }
