7d0d8c4671043003925662574184082fc4bcb000
Asher <ash@coder.com>
2023-10-16 11:19:51 -0800

    Fix DataGen not quite mirroring toAgentModels()
    It should omit agent information when the workspace specifies no agents
    and the name should combine the workspace and agent names.

--- src/test/groovy/DataGen.groovy
+++ src/test/groovy/DataGen.groovy
@@ -5,12 +5,19 @@ import com.coder.gateway.sdk.v2.models.WorkspaceStatus
 import com.coder.gateway.sdk.v2.models.WorkspaceTransition
 
 class DataGen {
-    static WorkspaceAgentModel workspace(String name, String workspaceName = name) {
+    // Create a random workspace agent model.  If the workspace name is omitted
+    // then return a model without any agent bits, similar to what
+    // toAgentModels() does if the workspace does not specify any agents.
+    // TODO: Maybe better to randomly generate the workspace and then call
+    //       toAgentModels() on it.  Also the way an "agent" model can have no
+    //       agent in it seems weird; can we refactor to remove
+    //       WorkspaceAgentModel and use the original structs from the API?
+    static WorkspaceAgentModel workspace(String name, String workspaceName = "", UUID agentId = UUID.randomUUID()) {
         return new WorkspaceAgentModel(
+                workspaceName == "" ? null : agentId,
                 UUID.randomUUID(),
-                UUID.randomUUID(),
-                workspaceName,
-                name,
+                workspaceName == "" ? name : workspaceName,
+                workspaceName == "" ? name : (workspaceName + "." + name),
                 UUID.randomUUID(),
                 "template-name",
                 "template-icon-path",
