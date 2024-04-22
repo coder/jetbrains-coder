999e05c59047d04358be6b01976b3acb7803b891
Asher <ash@coder.com>
2024-04-22 13:51:13 -0800

    Remove redundant semicolons

--- src/main/kotlin/com/coder/gateway/services/CoderRestClientService.kt
+++ src/main/kotlin/com/coder/gateway/services/CoderRestClientService.kt
@@ -1,11 +1,11 @@
-package com.coder.gateway.services;
+package com.coder.gateway.services
 
 import com.coder.gateway.sdk.CoderRestClient
 import com.coder.gateway.sdk.ProxyValues
 import com.intellij.ide.plugins.PluginManagerCore
 import com.intellij.openapi.components.service
-import com.intellij.openapi.extensions.PluginId;
-import com.intellij.util.net.HttpConfigurable;
+import com.intellij.openapi.extensions.PluginId
+import com.intellij.util.net.HttpConfigurable
 import okhttp3.OkHttpClient;
 import java.net.URL
 
