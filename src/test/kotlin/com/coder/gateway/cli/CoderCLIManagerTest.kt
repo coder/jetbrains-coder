329ee127ffa0ce014c399f704f7a585ff08ecddc
Garrett Delfosse <garrett@coder.com>
2024-06-25 15:17:02 +0000

    feature tests

--- src/test/kotlin/com/coder/gateway/cli/CoderCLIManagerTest.kt
+++ src/test/kotlin/com/coder/gateway/cli/CoderCLIManagerTest.kt
@@ -722,7 +722,8 @@ internal class CoderCLIManagerTest {
         val tests =
             listOf(
                 Pair("2.5.0", Features(true)),
-                Pair("4.9.0", Features(true)),
+                Pair("2.13.0", Features(true, true)),
+                Pair("4.9.0", Features(true, true)),
                 Pair("2.4.9", Features(false)),
                 Pair("1.0.1", Features(false)),
             )
